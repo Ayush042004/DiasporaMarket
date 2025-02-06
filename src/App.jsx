7a5d2fdfbc98641f5b69b67be13a7ab1218ab006
Ayush kumar <kumarayush042004@gmail.com>
2025-02-06 12:10:46 +0530

    Initialsed repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NavBar from './components/Header/NavBar'
+
+
+function App() {
+  
+
+  return (
+    <>
+       <NavBar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className=''>
+      
+        <div className='flex items-center py-1 bg-black'>
+          <p className=' text-[14px] text-white .poppins-extralight pl-5'> Get flat 40% off using aadhaar card</p>
+
+          <div>
+            
+          </div>
+
+        </div>
+
+      </div>
+
+  )
+}
+
+export default NavBar
