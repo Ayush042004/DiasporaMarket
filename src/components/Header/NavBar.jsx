83bf9fc59e6b2884be242cf0d0f2b2187e69231d
Ayush kumar <kumarayush042004@gmail.com>
2025-02-06 21:01:35 +0530

    initilal commit

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
+  const[language,setLanguage] = useState("en")
   return (
     <div className=''>
       
-        <div className='flex items-center py-1 bg-black'>
+        <div className=' flex items-center py-1 bg-black '>
           <p className=' text-[14px] text-white .poppins-extralight pl-5'> Get flat 40% off using aadhaar card</p>
-
-          <div>
-            
+          <div className=' flex items-center justify-end text-white ml-auto mr-[200px]'>
+            <ul>
+              <li className='poppins-extralight text-[13px] '>
+                <Link to="/help-centre">Help Center</Link>
+              </li>
+            </ul>
+            <select
+            className="bg-gray-800 ml-7 text-white text-[12px] border border-gray-600  rounded-md focus:outline-none "
+            value={language}
+            onChange={(e) => setLanguage(e.target.value)}>
+            <option value="en">English</option>
+            <option value="hi">हिन्दी (Hindi)</option>
+            <option value="es">Español (Spanish)</option>
+            <option value="fr">Français (French)</option>
+          </select>
           </div>
-
         </div>
 
       </div>
